bf3b1d31c9d4ea4b49cb7bd1880cf244dd199ddf
Supadchaya <138070207+spcyppt@users.noreply.github.com>
2025-01-30 23:37:37 -0800

    Update pt2_autograd_utils.cpp

--- fbgemm_gpu/codegen/training/pt2/pt2_autograd_utils.cpp
+++ fbgemm_gpu/codegen/training/pt2/pt2_autograd_utils.cpp
@@ -19,17 +19,14 @@ namespace fbgemm_gpu {
 
 Tensor reshape_vbe_output(
     const Tensor& grad_output,
-    const Tensor& B_offsets,
+    const c10::SymInt max_B,
     const Tensor& B_offsets_rank_per_feature,
     const Tensor& D_offsets) {
   /* FOR CPU VBE to use the same backend */
   const auto T = D_offsets.numel() - 1;
-  int32_t max_B = 0;
   int32_t total_D = 0;
-  // find max_B, total_D to create output [max_B, total_D]
+  // find total_D to create output [max_B, total_D]
   for (int32_t t = 0; t < T; t++) {
-    auto b = B_offsets[t + 1].item<int32_t>() - B_offsets[t].item<int32_t>();
-    max_B = std::max(max_B, b);
     total_D += D_offsets[t + 1].item<int32_t>() - D_offsets[t].item<int32_t>();
   }
   auto grad_output_ = at::empty({max_B, total_D}, grad_output.options());
@@ -61,16 +58,17 @@ Tensor reshape_vbe_output(
 }
 Tensor reshape_offsets(
     const Tensor& offsets,
-    const Tensor& B_offsets,
+    const Tensor& B_offsets_rank_per_feature,
     const c10::SymInt max_B,
     const int32_t T) {
     auto offsets_ = at::empty({T * max_B + 1}, offsets.options());
+    auto begin = 0;
     for (int32_t t = 0; t < T; t++) {
-        const auto b_begin = B_offsets[t];
-        const auto b_end = B_offsets[t+1];
+        const auto end = B_offsets_rank_per_feature[t][B_offsets_rank_per_feature[t].numel()-1] + begin;
         const auto values = offsets.slice(0, begin, end);
         offsets_.index_put_({t * max_B, t * max_B + end - begin}, values);
         offsets_[t * max_B + end - begin : (t + 1) * max_B] = offsets_[end];
+        begin = end;
     }
     offsets_[offsets.numel()-1] = offsets[offsets.numel()-1];
     return offsets_;
